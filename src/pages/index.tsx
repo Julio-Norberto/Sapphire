df59d5d66a62028388bcb64bebe064d9de851f08
Julio-Norberto <juliocsscl@gmail.com>
2023-04-26 22:51:29 -0300

    feat: create Footer component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
-import { ABeeZee } from 'next/font/google'
 import styles from '@/styles/Home.module.scss'
 
-import { Header } from '../components/header'
+import { Header } from '@/components/header'
+import { Footer } from '@/components/Footer'
 
 export default function Home() {
   return (
     <main>
       <Header title={`Conteúdo bruto entregue para você de maneira totalmente lapidada`} />
       <h1 className={styles.title}>main</h1>
+      <Footer />
     </main>
   )
 }
