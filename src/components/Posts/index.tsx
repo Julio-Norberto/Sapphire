c6d2a8f02f7bff563494af3573bcd0c2734da6be
Julio-Norberto <juliocsscl@gmail.com>
2023-04-28 12:57:21 -0300

    feat: create component Posts

---
+++ src/components/Posts/index.tsx
@@ -0,0 +1,47 @@
+import styles from '../../styles/Posts.module.scss'
+
+import image1 from '../../../public/oazulEbranco.png'
+import image2 from '../../../public/verdeEbranco.png'
+import image3 from '../../../public/roxoEbranco.png'
+
+import { PostCard } from '../postCard'
+
+
+interface IPosts {
+  image: any
+  category: string
+  title: string
+  summary: string
+}
+
+// Temporary Mock
+const posts: IPosts[] = [
+  {
+    image: image1.src,
+    category: 'TypeScript',
+    title: 'Usando TypeScript POO da forma certa',
+    summary: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita corporis sit alias optio doloremque praesentium'
+  },
+  {
+    image: image2.src,
+    category: 'Back-end',
+    title: 'Multi Threading com Go',
+    summary: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita corporis sit alias optio doloremque praesentium'
+  },
+  {
+    image: image3.src,
+    category: 'Banco de dados',
+    title: 'Prisma - O Guia definitivo',
+    summary: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita corporis sit alias optio doloremque praesentium'
+  },
+]
+
+export const Posts = () => {
+  return (
+    <main className={styles.postsContainer} >
+      { posts.map((post) => (
+        <PostCard image={post.image} category={post.category} title={post.title} summary={post.summary} />
+      )) }
+    </main>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import styles from '@/styles/Home.module.scss'
 
 import { Header } from '@/components/header'
-import { PostCard } from '@/components/postCard'
+import { Posts } from '@/components/Posts'
 import { Footer } from '@/components/Footer'
 
 export default function Home() {
   return (
     <main>
       <Header title={`Conteúdo bruto entregue para você de maneira totalmente lapidada`} />
+      <Posts />
       <Footer />
     </main>
   )
