52e5bb9d398a87c96c8963e6b563f33cb9f997bf
Julio-Norberto <juliocsscl@gmail.com>
2023-05-09 11:16:54 -0300

    feat: create author page

--- src/pages/author/[id].tsx
+++ src/pages/author/[id].tsx
@@ -1,11 +1,25 @@
+import { Header } from '@/components/header'
+import styles from '../../styles/AuthorPage.module.scss'
+
 import { useRouter } from "next/router"
+import { Footer } from '@/components/Footer'
 
 export default function Author() {
   const { query } = useRouter()
 
   return (
     <div>
-      <h1>Author {query.id} </h1>
+      <header>
+        <Header />
+      </header>
+
+      <div className={styles.posts}>
+
+      </div>
+
+      <footer>
+        <Footer />
+      </footer>
     </div>
   )
 }
