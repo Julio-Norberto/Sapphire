d617e91788851c9d0eddfdf5e7e5449782cf1bd8
Julio-Norberto <juliocsscl@gmail.com>
2023-05-11 08:20:43 -0300

    feat: add component Loading on page post

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -7,6 +7,7 @@ import { Footer } from "@/components/Footer"
 
 import { Variables } from "graphql-request"
 import { loadPostById } from '@/api/loadPostById'
+import { Loading } from '@/components/Loading'
 
 interface IPostsProps {
   attributes: {
@@ -29,6 +30,7 @@ export default function Post() {
   const [posts, setPosts] = useState<IPostsProps>()
   const [author, setAuthor] = useState<Author>()
   const [authorId, setAuthorId] = useState<string>()
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const id: Variables = {
@@ -39,6 +41,7 @@ export default function Post() {
       setPosts(response.post.data)
       setAuthor(response.post.data.attributes.author.data.attributes.photo.data.attributes)
       setAuthorId(response.post.data.attributes.author.data.id)
+      setLoading(false)
     }).catch((error) => {
       console.log(error)
       console.clear()
@@ -48,7 +51,7 @@ export default function Post() {
 
   return (
     <div>
-      <Header title={posts ? posts.attributes.title : ''} />
+      <Header component={ loading ? <Loading /> : '' } title={posts ? posts.attributes.title : ''} />
 
       <div className={styles.postPageContainer} >
         <span> { posts? posts.attributes.createdAt.slice(0, 10) : '' } </span>
@@ -59,11 +62,11 @@ export default function Post() {
 
         <div className={styles.postAuthor} >
           <div>
-            <img src={author?.url} alt={`${author?.alternativeText}`} />
+            <img src={author ? author?.url : ''} alt={`${author ? author?.alternativeText : ''}`} />
           </div>
 
           <div>
-            <h3 onClick={() => push(`/author/${authorId}`)} > { posts? posts.attributes.authorName : 'John Doe' } </h3>
+            <h3 onClick={() => push(`/author/${authorId}`)} > { posts? posts.attributes.authorName : '' } </h3>
             <p> { author?.authorTitle ? author.authorTitle : 'Software Developer' } </p>
           </div>
         </div>
