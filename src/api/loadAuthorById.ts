833d8ec5990a3c7ab3bd9d2c4bac0c11172e51b2
Julio-Norberto <juliocsscl@gmail.com>
2023-05-13 20:39:41 -0300

    feat: change URL from API graphql

--- src/api/loadAuthorById.ts
+++ src/api/loadAuthorById.ts
@@ -1,8 +1,10 @@
 import { Variables, request } from 'graphql-request'
 import { GETAUTHORBYID } from '../graphql/queries'
 
+import { graphqlURL } from '@/config/config'
+
 export const loadAuthorById = async(id: Variables) => {
-    const data = await request('http://localhost:1337/graphql', GETAUTHORBYID, id)
+    const data = await request(graphqlURL, GETAUTHORBYID, id)
 
     return data
 }
--- src/api/loadPostById.ts
+++ src/api/loadPostById.ts
@@ -1,8 +1,12 @@
 import { Variables, request } from 'graphql-request'
 import { GETPOSTBYID } from '../graphql/queries'
 
+import { graphqlURL } from '@/config/config'
+
+// 'http://localhost:1337/graphql'
+
 export const loadPostById = async(id: Variables) => {
-  const data = await request('http://localhost:1337/graphql', GETPOSTBYID, id)
+  const data = await request(graphqlURL, GETPOSTBYID, id)
 
   return data
 }
