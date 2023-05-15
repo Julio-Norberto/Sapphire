6f5d5bf41248ca4222d5f0a4b5832eab983afa6e
Julio-Norberto <juliocsscl@gmail.com>
2023-05-15 08:22:01 -0300

    feat: create component NoData

---
+++ src/components/NoData/index.tsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/NoData.module.scss'
+import astronaut from '../../../public/Astronaut helmet-amico.svg'
+
+export const NoData = () => {
+  return(
+    <div className={styles.container} >
+      <h2>Ops... Ainda não temos Posts dessa categoria.</h2>
+      <p><a>Clique e continue desbravando o espaço do nosso conteúdo</a> </p>
+      <img src={astronaut.src} alt="imagem do capacete de um astronauta com detalhes azuis" />
+    </div>
+  )
+}
