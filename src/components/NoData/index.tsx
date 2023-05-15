c931b95d64fd8190850b6ba825b63037dbfd21bb
Julio-Norberto <juliocsscl@gmail.com>
2023-05-15 08:26:00 -0300

    feat: add link to component NoData

--- src/components/NoData/index.tsx
+++ src/components/NoData/index.tsx
@@ -5,7 +5,7 @@ export const NoData = () => {
   return(
     <div className={styles.container} >
       <h2>Ops... Ainda não temos Posts dessa categoria.</h2>
-      <p><a>Clique e continue desbravando o espaço do nosso conteúdo</a> </p>
+      <p><a href='/' >Clique e continue desbravando o espaço do nosso conteúdo</a> </p>
       <img src={astronaut.src} alt="imagem do capacete de um astronauta com detalhes azuis" />
     </div>
   )
